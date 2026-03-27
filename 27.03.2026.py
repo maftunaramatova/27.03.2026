b1f7bb71682740808eeea0e49ec7f2ce4e6ff3ae
senyoramoon010 <djumaniyazovumarjon@gmail.com>
2026-03-27 17:47:35 +0500

    Add files via upload

--- 27.03.2026.py
+++ 27.03.2026.py
@@ -15,5 +15,27 @@
 # Bobur = User("Bobur","Jovliyev",15)
 #
 # print(Bobur.get_age())
-# Bobur.set_age(25)
+# Bobur.set_age (25)
 # print(Bobur.get_age())
+# #-----------------------------------------
+# class User:
+#     def __init__(self, first_name, last_name,age):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.__age = age
+#     @property
+#     def get_age(self):
+#         return self.__age
+#     @get_age.setter
+#     def set_age(self, value):
+#         if value < 0:
+#             return ValueError("Yosh manfiy bo'lmasligi lozim!")
+#         self.__age = value
+#
+# Bobur = User("Bobur","Jovliyev",15)
+#
+# print(Bobur.get_age)
+# Bobur.set_age = 25
+# print(Bobur.get_age)
+
+
